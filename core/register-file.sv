2c54deb522312c5f7931955d9c382cb1c3e2e92b
Zharkynali Alikhan <tchnqxgithub@gmail.com>
2026-06-23 01:39:09 +0500

    Update register-file.sv

--- core/register-file.sv
+++ core/register-file.sv
@@ -1,3 +1,6 @@
+// Under GPL v3
+// Zharkynali Alikhan S.
+
 module register #(
     parameter WIDTH = 8
 )(
@@ -18,3 +21,27 @@ module register #(
     end
 
 endmodule
+
+
+// module general_regfile #(
+//     parameter WIDTH = 8,
+//     parameter WIDTH_REG_OUT_CHOICE_OPCODE = 2
+// )(
+//     input clk,
+//     input rst,
+//     input rw,
+
+//     input [WIDTH-1:0] input_from_alu_result,
+//     input [WIDTH-1:0] input_from_data_bus,
+//     input input_opcode,
+
+//     input [WIDTH_REG_OUT_CHOICE_OPCODE-1:0] reg_first_out_choice_opcode,
+//     input [WIDTH_REG_OUT_CHOICE_OPCODE-1:0] reg_second_out_choice_opcode,
+
+//     input [WIDTH_REG_OUT_CHOICE_OPCODE-1:0] 
+
+//     output [WIDTH-1:0] reg_first_out,
+//     output [WIDTH-1:0] reg_second_out
+// );
+
+// endmodule
